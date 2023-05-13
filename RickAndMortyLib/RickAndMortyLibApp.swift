e6f20b9761e75af474020ac8c731665a9a95fabe
jrobertt0 <robedeleong@gmail.com>
2023-05-13 12:52:58 -0600

    Initial Commit

---
+++ RickAndMortyLib/RickAndMortyLibApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyLibApp.swift
+//  RickAndMortyLib
+//
+//  Created by Jose Roberto de Leon Garcia on 13/05/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyLibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
